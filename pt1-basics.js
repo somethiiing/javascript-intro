2f53e3e5dd66b8a7152b78f362d788427cf7194a
Wilson Yu <hello@wilsonyu.io>
2017-08-13 19:59:26 -0400

    update for arrays

--- pt1-basics.js
+++ pt1-basics.js
@@ -14,9 +14,11 @@ When you're done with the entire lesson, run the code in Node.
 
 Chapter 1: The Basics.
 Here we're gonna work on basic Javascript syntax. Most of these will be simple variable assignments.
+                                                                                                                                                                    */
 
-Part 1: Strings:                                                                                                                            */
 
+
+// Part 1: Strings:
 // 1A. Using a 'let' statement, define the variable 'color' with your favorite color!
 //CODE// //CODE// //CODE//
 
@@ -35,6 +37,80 @@ const extraSpaces = '      so many extra spaces. DansGame.       ';
 const improperSpaces = 'please.turn.these.periods.into.spaces.'
 //CODE// //CODE// //CODE//
 //CODE// //CODE// //CODE//
+
+
+
+// Part 2: Numbers:
+// 2A. Define a variable 'age', which is your birthday.
+//CODE// //CODE// //CODE//
+
+// 2B. Using Math methods, find the minimum and maximum of the array. Define them in numbersMin and numbersMax;
+const numbersArr = [5, 6, 3, 4, 7, 8, 1, 2];
+//CODE// //CODE// //CODE//
+//CODE// //CODE// //CODE//
+
+// 2C. Find the absolute value of -47 and assign it to numbersAbs.
+//CODE// //CODE// //CODE//
+
+// 2D. Assign a random 4 digit number (no decimals!) to variable randomFourDigitNumber
+//CODE// //CODE// //CODE//
+
+
+
+// Part 3: Equality + Truthy/Falsey
+// 3. Assign variables equality1, equality2, etc and make them all true. (This might seem a little convoluted);
+// 3A. equality1: Use the less symbol.
+//CODE// //CODE// //CODE//
+
+// 3B. equality2: Use the greater than symbol.
+//CODE// //CODE// //CODE//
+
+// 3C. equality3: Use the triple equal sign.
+//CODE// //CODE// //CODE//
+
+// 3D. equality4: Use addition and the === sign.
+//CODE// //CODE// //CODE//
+
+// 3E. equality5: Use false.
+//CODE// //CODE// //CODE//
+
+// 3F. equality6: Use undefined.
+//CODE// //CODE// //CODE//
+
+
+
+// Part 4: Arrays
+// 4A. Create a variable colorsArray and put five colors in your array.
+//CODE// //CODE// //CODE//
+
+// 4B. Assign the third color in your array and assign it to favoriteColor.
+//CODE// //CODE// //CODE//
+
+// The next challenges will be using the moreNumbers. DO NOT ALTER the array.
+const moreNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// 4C. Use a method to reverse the array and set that to variable reversedNumbers.
+//CODE// //CODE// //CODE//
+
+// 4D. Use a method to return the fifth through 8th (inclusive) and assign it to someNumbers.
+//CODE// //CODE// //CODE//
+
+// 4E. Make a new copy of the array and assign it to newNumbers.
+//CODE// //CODE// //CODE//
+
+// 4F. Add the number 11 to the end of newNumbers.
+//CODE// //CODE// //CODE//
+
+// 4F. Add the number -2 and -1 to the front of newNumbers.
+//CODE// //CODE// //CODE//
+
+
+
+
+// Part 5: Objects
+
+
+
+
 
 
 
@@ -64,3 +140,29 @@ let evenHappier = happy.toUpperCase();
 let noExtraSpaces = extraSpaces.trim();
 let properSpaces1 = improperSpaces.split('.').join(' ');
 let properSpaces2 = improperSpaces.replace('.', ' ');
+
+//Numbers
+let age = 25;
+let numbersMin = Math.min(numbersArr);
+let numbersMax = Math.max(numbersArr);
+let numbersAbs = Math.abs(-47);
+let randomFourDigitNumber = Math.round(Math.random() * 100);
+
+//Equality
+let equality1 = 5 < 10;
+let equality2 = 10 >= 5;
+let equality3 = 3 === 3;
+let equality4 = (3 + 6) === 9;
+let equality5 = !false;
+let equality6 = (!undefined);
+
+//Arrays
+let colorsArray = ['white', 'blue', 'black', 'red', 'green'];
+let favoriteColor = colorsArray[2];
+let reversedNumbers = moreNumbers.reverse();
+let someNumbers = moreNumbers.slice(5, 9);
+let newNumbers = moreNumbers.slice(0);
+newNumbers.push(11);
+newNumbers.unshift(-2, -1);
+
+//
