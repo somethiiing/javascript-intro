1fe5d259c3adf91a4f37b2121edb99d3a99d3238
Wilson Yu <hello@wilsonyu.io>
2017-10-10 09:49:26 -0400

    add algos

--- pt3-algorithms.js
+++ pt3-algorithms.js
@@ -328,16 +328,169 @@ let paintCan = (matrix, target, color) => {
 
 
 
+/*
+13. Shuffle Deck
+
+Given an array containing a deck of cards, implement a function that shuffles the deck.
+
+Example:
+  let deck = orderedDeckCreator();
+  // ["A♥","2♥","3♥",...,"J♦","Q♦","K♦"]
+  shuffleDeck(deck);
+  // ["2♠","J♣","A♦", ... ,"7♣","8♣","K♠"]
+
+
+Note:
+  A shuffled deck should be completely random. That means that a given card should
+  be as likely as any other to appear in a given deck index, completely independent
+  of the order of the input deck. Think carefully about how to be sure your algorithm
+  generates a properly shuffled deck-- it is easy to accidentally create a biased algorithm.
+
+A further note on randomness:
+  Technically, a computer-shuffled deck will usually be "pseudorandom",
+  not "truly" random. However, the difference between the two is too small to
+  be detectable by any known test.
+  See http://en.wikipedia.org/wiki/Pseudorandom_number_generator .
+
+*/
+
+// orderedDeckCreator creates a perfectly ordered deck.
+let orderedDeckCreator = () => {
+  const suits = [ '♥', '♣', '♠', '♦' ];
+  const values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
+  let deck = [];
+
+  suits.forEach(suit => {
+    values.forEach(value => {
+      deck.push(value + suit);
+    });
+  });
+
+  return deck;
+};
+
+let shuffleDeck = deck => {
+//   CODE GOES HERE   // //   CODE GOES HERE   // //   CODE GOES HERE   //
+};
 
 
 
 
+/*
+14. Roman Numeral Translator
+
+Given a roman numeral as input, write a function that converts the roman numeral to a number and outputs it.
+
+Ex: translateRomanNumeral("LX") ===>>> 60
+
+When a smaller numeral appears before a larger one, it becomes
+a subtractive operation. You can assume only one smaller numeral
+may appear in front of larger one.
+
+Ex:
+translateRomanNumeral("IV") ===>>> 4
+
+You should return `null` on invalid input.
+*/
+
+const romanNumeralDigitValues = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+let translateRomanNumeral = romanNumeral => {
+//   CODE GOES HERE   // //   CODE GOES HERE   // //   CODE GOES HERE   //
+};
 
 
 
+/*
+15. Merge Sort
 
+Implement a function that sorts an array of numbers using the "mergesort" algorithm.
 
+Mergesort is an optimized sorting algorithm which is a common choice to implement `sort`
+methods in standard libraries as an alternative to quicksort or heapsort. (For example,
+Firefox's Array.sort method uses a tuned mergesort; the WebKit engine used by Chrome and
+Safari uses quicksort for numeric arrays, and mergesort for arrays of strings.)
 
+Mergesort uses a divide-and-conquer strategy. It begins by treating the input list of length N
+as a set of N "sublists" of length 1, which are considered to be sorted. Adjacent sublists are then
+"merged" into sorted sublists of length 2, which are merged into sorted sublists of length 4, and so
+on, until only a single sorted list remains. (Note, if N is odd, an extra sublist of length 1 will be left
+after the first merge, and so on.)
+
+This can be implemented using either a recursive ("top-down") or an iterative ("bottom-up") approach.
+
+Illustration of an iterative approach:
+
+  Initial step: Input array is split into "sorted" sublists
+  [4,7,4,3,9,1,2] -> [[4],[7],[4],[3],[9],[1],[2]]
+
+  Merge step: Adjacent sublists are merged into sorted sublists
+  [[4],[7],[4],[3],[9],[1],[2]] -> [[4,7],[3,4],[1,9],[2]]
+
+  Repeat merge step:
+  [[4,7],[3,4],[1,9],[2]] -> [[3,4,4,7], [1,2,9]]
+
+  Repeat merge step:
+  [[3,4,4,7], [1,2,9]] -> [[1,2,3,4,4,7,9]]
+
+  Done! Return the sorted array:
+  [1,2,3,4,4,7,9]
+Illustration of a recursive approach:
+
+  1. Split the input array in half
+  [4, 7, 4, 3, 9, 1, 2] -> [4, 7, 4], [3, 9, 1, 2
+
+  2. Both sides are sorted recursively:
+  [4, 7, 4] -> [4, 4, 7]
+  [3, 9, 1, 2] -> [1, 2, 3, 9]
+
+  3. Both halves are merged:
+  [4, 7, 4], [3, 9, 1, 2] -> [1, 2, 3, 4, 4, 7, 9]
+
+  Step 2 might seem a bit mystical - how do we sort both sides? The
+  simple answer is that we use mergesort! After all, mergesort sorts
+  arrays, right? We can test this on [4, 7, 4] by just following the
+  steps above but imagining that [4, 7, 4] is the whole array, which
+  is what happens when you call mergesort on it.
+
+  1. Split the input array in half
+  [4, 7, 4] -> [4], [7, 4]
+
+  2. Both sides are sorted recursively:
+  [4] -> [4]
+  [7, 4] -> [4, 7]
+
+  3. Both halves are merged:
+  [4], [4, 7] -> [4, 4, 7]
+
+  I cheated again by going directly from [7, 4] to [4, 7], but that's
+  really just:
+
+  1. Split the input array in half
+  [7, 4] -> [7], [4]
+
+  2. Both sides are sorted recursively:
+  [7] -> [7]
+  [4] -> [4]
+
+  3. Both halves are merged:
+  [7], [4] -> [4, 7]
+
+  As you can see, all the work actually gets done in step 3, the merge
+  step. Everything else is just splitting and recursing.
+*/
+
+let mergeSort = arr => {
+//   CODE GOES HERE   // //   CODE GOES HERE   // //   CODE GOES HERE   //
+};
 
 
 
@@ -366,5 +519,10 @@ module.exports = {
   isPalindrome: isPalindrome,
   longestPalindrome: longestPalindrome,
   rotateMatrix: rotateMatrix,
-  paintCan: paintCan
+  paintCan: paintCan,
+  orderedDeckCreator: orderedDeckCreator,
+  shuffleDeck: shuffleDeck,
+  romanNumeralDigitValues: romanNumeralDigitValues,
+  translateRomanNumeral: translateRomanNumeral,
+  mergeSort: mergeSort
 };
